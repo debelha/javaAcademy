1a6810d8e98cc1a9528250d81ec13d24e6f35fb3
unknown <dleitedias@gmail.com>
2024-06-08 21:24:09 -0300

    Exercício 1

---
+++ 2- Exercícios para entrega (classes, objetos e construtores)/1-Exercicio/src/main/java/pacote1/Biblioteca.java
@@ -0,0 +1,25 @@
+package pacote1;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Biblioteca {
+
+    private List<Livro> livros;
+
+    public Biblioteca() {
+        this.livros = new ArrayList<>();
+    }
+
+    public void adicionarLivro(Livro livro) {
+        livros.add(livro);
+    }
+
+    public void listarLivros() {
+        for (Livro livro : livros) {
+            System.out.println("Título: " + livro.getTitulo());
+            System.out.println("Autor: " + livro.getAutor());
+            System.out.println("Ano: " + livro.getAno());
+            System.out.println();
+        }
+    }
+}
