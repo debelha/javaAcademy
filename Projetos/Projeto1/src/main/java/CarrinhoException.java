7447006a2a13000c37560d3a927f01e032f055a5
unknown <dleitedias@gmail.com>
2024-06-12 21:12:21 -0300

    Reset diretórios

--- Projetos/Projeto1/src/main/java/CarrinhoException.java
+++
@@ -1,9 +0,0 @@
-public class CarrinhoException extends Exception {
-    public CarrinhoException(String message) {
-        super(message);
-    }
-
-    public CarrinhoException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
