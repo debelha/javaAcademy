7f0b5fada46fe7aa1232766e4888316a8c760160
unknown <dleitedias@gmail.com>
2024-06-23 21:51:06 -0300

    Classes abstratas

---
+++ Java/Atividades de fixação/ClassesAbstratas/src/main/java/Animal.java
@@ -0,0 +1,8 @@
+public abstract class Animal {
+
+    public abstract void fazerBarulho();
+
+    public void andar(){
+        System.out.println("Animal andando");
+    }
+}
---
+++ Java/Atividades de fixação/ClassesAbstratas/src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        gato.fazerBarulho();
+
+        Cachorro cachorro = new Cachorro();
+        cachorro.fazerBarulho();
+    }
+}
