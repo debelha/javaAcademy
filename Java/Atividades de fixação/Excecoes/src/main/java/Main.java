36088c196812feca75809a60240658a2fbd3536a
unknown <dleitedias@gmail.com>
2024-06-24 16:58:01 -0300

    Exceptions: exemplo 1

---
+++ Java/Atividades de fixação/Excecoes/src/main/java/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        a(10);
+    }
+
+    public static void a(int i) throws Exception {
+        System.out.println("Iniciando execução de A");
+        b(i);
+        System.out.println("Terminando execução de A");
+    }
+
+    public static void b(int i) throws Exception {
+        System.out.println("Iniciando execução de B");
+        if(i==0)
+            throw new Exception("Lançcando exceção em B");
+
+        System.out.println("Terminando execução de B");
+    }
+}
